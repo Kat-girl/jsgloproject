085051c97a882388277769baa2d78306206727e4
Kat-girl <68987747+Kat-girl@users.noreply.github.com>
2022-03-20 02:47:10 +0300

    Add files via upload

---
+++ js/tabs.js
@@ -0,0 +1,53 @@
+const tabButtons = document.querySelectorAll('.design-list__item');
+const tabDescriptions = document.querySelectorAll('.design__descr');
+const tabImages = document.querySelectorAll('.design-images');
+const tabFeatureImages = document.querySelectorAll('.design-block__img');
+const tabDesingnTitles = document.querySelectorAll('.design__title');
+
+console.log(tabDesingnTitles);
+
+
+const changeContent = (array, value) => {
+    array.forEach((elem) => {
+            if (elem.dataset.tabsField === value) {
+                elem.classList.remove('hidden');
+            } else {
+                elem.classList.add('hidden');
+            }
+        })
+}
+
+tabButtons.forEach((tabButton, index) => {
+    tabButton.addEventListener('click', (event) => {
+        const dataValue = tabButton.dataset.tabsHandler;
+        
+        changeContent(tabImages, dataValue);
+        changeContent(tabDescriptions, dataValue);
+        changeContent(tabFeatureImages, dataValue);  //ДЗ день 1
+
+        
+
+        tabDesingnTitles.forEach((title, indexTitle) => {   //ДЗ день 1
+            if (index === indexTitle) {
+                title.classList.remove('hidden');
+                document.title = title.textContent;
+            } else {
+                title.classList.add('hidden');
+            }
+        })
+
+        tabButtons.forEach((btn) => {
+        
+            if (btn === event.target) {
+                btn.classList.add('design-list__item_active');
+            } else {
+                btn.classList.remove('design-list__item_active');
+            }
+            
+        })
+        
+    })
+
+    
+})
+
