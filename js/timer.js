7b0c767c645b15b0b2c51eb3a7a7ea0cc5308f4f
Kat-girl <68987747+Kat-girl@users.noreply.github.com>
2022-03-26 16:25:15 +0300

    Add files via upload

--- js/timer.js
+++ js/timer.js
@@ -1,9 +1,31 @@
 const timerBlock = document.querySelector('.timer__time');
-const date = new Date();
-const hours = date.getHours();
-const minutes = date.getMinutes();
-const seconds = date.getSeconds();
+const deadline = '26 march 2022';
+let interval;
 
-console.log(`${hours}:${minutes}:${seconds}`);
 
-timerBlock.textContent = `${hours}:${minutes}:${seconds}`;
+const updateClock = () => {
+    const date = new Date().getTime();
+    const dateDeadline = new Date(deadline).getTime();
+    const timeRemaining = (dateDeadline - date) / 1000;
+    const hours = Math.floor(timeRemaining / 60 / 60);
+    const minutes = Math.floor((timeRemaining / 60) % 60);
+    const seconds = Math.floor(timeRemaining % 60);
+
+    const fHours = hours < 10 ? '0' + hours : hours;
+    const fMinutes = minutes < 10 ? '0' + minutes : minutes;
+    const fSeconds = seconds < 10 ? '0' + seconds : seconds;
+    
+    timerBlock.textContent = `${fHours}:${fMinutes}:${fSeconds}`;
+
+    if (timeRemaining <= 0) {
+        clearInterval(interval);
+        timerBlock.textContent = '00:00:00';
+    }
+}
+
+updateClock();
+interval = setInterval(updateClock, 500);
+
+
+
+
