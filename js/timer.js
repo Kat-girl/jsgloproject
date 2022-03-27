4fa6b7310e64f46a5e017a881739f8f8e3480192
Kat-girl <68987747+Kat-girl@users.noreply.github.com>
2022-03-27 17:26:17 +0300

    Add files via upload

--- js/timer.js
+++ js/timer.js
@@ -1,5 +1,5 @@
 const timerBlock = document.querySelector('.timer__time');
-const deadline = '26 march 2022';
+const deadline = '31 march 2022';
 let interval;
 
 
@@ -8,14 +8,31 @@ const updateClock = () => {
     const dateDeadline = new Date(deadline).getTime();
     const timeRemaining = (dateDeadline - date) / 1000;
     const hours = Math.floor(timeRemaining / 60 / 60);
+    const hoursWDays = Math.floor(hours % 24);
     const minutes = Math.floor((timeRemaining / 60) % 60);
     const seconds = Math.floor(timeRemaining % 60);
-
-    const fHours = hours < 10 ? '0' + hours : hours;
+    let days = Math.floor(hours / 24);
+   
+    const fHours = hoursWDays < 10 ? '0' + hoursWDays : hoursWDays;
     const fMinutes = minutes < 10 ? '0' + minutes : minutes;
     const fSeconds = seconds < 10 ? '0' + seconds : seconds;
+    let dateWord;
     
-    timerBlock.textContent = `${fHours}:${fMinutes}:${fSeconds}`;
+         if (days == 1) {
+            dateWord = "день";
+        } else if ((days > 1) && (days < 5)) {
+            dateWord = "дня";
+         } else if (days == 0) {
+             dateWord = '';
+             days = '';
+         }
+         else {
+            dateWord = "дней";
+        }
+       
+        
+    
+    timerBlock.textContent = `${days} ${dateWord} ${fHours}:${fMinutes}:${fSeconds}`;
 
     if (timeRemaining <= 0) {
         clearInterval(interval);
