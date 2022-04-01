be66af3af3362a0431b5b22ac15f146fedde3ad9
Kat-girl <68987747+Kat-girl@users.noreply.github.com>
2022-04-01 23:14:30 +0300

    Add files via upload

--- js/sendForm.js
+++ js/sendForm.js
@@ -1,25 +1,39 @@
-const form = document.querySelector(".form-test-drive");
+const forms = document.querySelectorAll("form");
 
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
+forms.forEach((form) => {
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
 
-  const formDate = new FormData(form);
-  const body = {};
-  formDate.append("form", form.classList.value);
-  formDate.forEach((value, field) => {
-    body[field] = value;
-  });
+    const formDate = new FormData(form);
+    const body = {};
+    formDate.append("form", form.classList.value);
+    formDate.forEach((value, field) => {
+      body[field] = value;
+    });
 
-  fetch("https://jsonplaceholder.typicode.com/posts", {
-    method: "POST",
-    body: JSON.stringify(body),
-    headers: {
-      "Content-type": "application/json; charset=UTF-8",
-    },
-  }).then((response) => {
-    return response.json();
-  });
-    .then((data) => {
-        console.log(data)
+    fetch("https://jsonplaceholder.typicode.com/posts", {
+      method: "POST",
+      body: JSON.stringify(body),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
     })
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error(response.status);
+        }
+      })
+      .then((data) => {
+        alert("Success");
+      })
+      .catch((error) => {
+        console.log(error.message);
+        alert("Error " + error.message);
+      })
+      .finally(() => {
+        form.reset();
+      });
+  });
 });
